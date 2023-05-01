f1faa7498befc1f4b02f03e88a0c3e1dc8ad3a3d
Anders Lundsby <andersls@mvb.net>
2023-05-01 09:54:44 +0200

    isexyourmom

--- singleboxes.gd
+++ singleboxes.gd
@@ -22,8 +22,8 @@ onready var depthvalue = $"../../depth"
 func _button_pressed(boxid,listobox):
 #	print("gays count to"+String(listobox[boxid-1]["id"]))
 	for i in range(listobox.size()):
-		listobox[i]["high"]=false
-	listobox[boxid-1]["high"]=true
+		listobox[i]["high"] = false
+	listobox[boxid-1]["high"] = true
 	xvalue.value = listobox[boxid-1]["pos"].x 
 	yvalue.value = listobox[boxid-1]["pos"].y
 	zvalue.value = listobox[boxid-1]["pos"].z 
@@ -33,7 +33,7 @@ func _button_pressed(boxid,listobox):
 
 func createnewlistitem(boxid,listobox):
 	newPanel = Panel.new()
-	newPanel.rect_min_size=Vector2(100,50)
+	newPanel.rect_min_size = Vector2(100,50)
 	add_child(newPanel)
 	
 	newVcont=VBoxContainer.new()
@@ -41,27 +41,27 @@ func createnewlistitem(boxid,listobox):
 	newPanel.add_child(newVcont)
 
 	newHcont = HBoxContainer.new()
-	newHcont.rect_min_size=Vector2(100,25)
+	newHcont.rect_min_size = Vector2(100,25)
 	newVcont.add_child(newHcont)
 	
-	newLabel=Label.new()
-	newLabel.rect_min_size=Vector2(50,25)
-	newLabel.text=String(boxid)
-	newLabel.align=1
-	newLabel.valign=1
+	newLabel = Label.new()
+	newLabel.rect_min_size = Vector2(50,25)
+	newLabel.text = String(boxid)
+	newLabel.align = 1
+	newLabel.valign = 1
 	newHcont.add_child(newLabel)
 	
-	newColor =ColorPickerButton.new()
-	newColor.rect_min_size=Vector2(50,25)
-	newColor.color=listobox[boxid-1]["color"]
+	newColor = ColorPickerButton.new()
+	newColor.rect_min_size = Vector2(50,25)
+	newColor.color = listobox[boxid-1]["color"]
 	newHcont.add_child(newColor)
 	
-	newButt=Button.new()
-	newButt.rect_min_size=Vector2(100,25)
-	newButt.text=listobox[boxid-1]["name"]
+	newButt = Button.new()
+	newButt.rect_min_size = Vector2(100,25)
+	newButt.text = listobox[boxid-1]["name"]
 	newButt.connect("pressed", self, "_button_pressed",[boxid,listobox])
 	newVcont.add_child(newButt)
-	
-		
+
+
 func _on_Spatial_newlistitem(boxid,listobox):
 	createnewlistitem(boxid,listobox)
